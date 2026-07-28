d485f9ba6bb134ece0d62c1f3cf6cc1962be82c6
Vector05 <mathurraghav23@gmail.com>
2026-07-28 18:09:02 +0530

    4.2-1

--- foundations/4-2-1.cpp
+++ foundations/4-2-1.cpp
@@ -159,17 +159,13 @@ void printMatrix(const Matrix& mat) {
 int main() {
     // Example with 4x4 matrices (n must be a power of 2)
     Matrix A = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12},
-        {13, 14, 15, 16}
+        {1, 3},
+        {7, 5}
     };
 
     Matrix B = {
-        {1, 0, 0, 0},
-        {0, 1, 0, 0},
-        {0, 0, 1, 0},
-        {0, 0, 0, 1}
+        {6, 8},
+        {4, 2}
     };
 
     std::cout << "Matrix A * B (Identity Matrix):\n";
